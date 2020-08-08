44e38c5fce0c2a10935f4ac0e3a48adc5046ae99
Lucas <lffsantos@gmail.com>
2020-08-08 02:41:21 -0300

    routes

--- cashback_system/urls.py
+++ cashback_system/urls.py
@@ -1,21 +1,26 @@
-"""cashback_system URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Base Service API",
+      default_version='v1',
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="test@example.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/', include('core.urls')),
+    path('login/', obtain_jwt_token),
+    path('refresh-token/', refresh_jwt_token),
 ]
