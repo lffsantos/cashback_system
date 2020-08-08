33cd354a5b74c01ce5d1f13ebc37bff873e52b1c
Lucas <lffsantos@gmail.com>
2020-08-08 18:53:05 -0300

    adjus serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,36 +1,34 @@
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
 
 from core.models import Dealer, Purchase
-from core.validators import validate_cpf
+from core.validators import validate_cpf, validate_dealer
 
 
 class DealerSerializer(serializers.ModelSerializer):
-    cpf = serializers.CharField(validators=[validate_cpf, UniqueValidator(queryset=Dealer.objects.all())])
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = Dealer
-        exclude = ['user']
-
-    def validate_cpf(self, data):
-        user = User.objects.filter(username=data)
-        if user:
-            raise serializers.ValidationError("Já existe usuário com esse CPF")
-
-        return data
+        fields = ('id', 'name', 'cpf', 'email', 'password')
 
     def create(self, validated_data):
-        username = validated_data['cpf']
-        password = make_password(validated_data.pop('password'))
-        user = User.objects.create(username=username, password=password)
-        dealer = Dealer.objects.create(user=user, **validated_data)
+        email, name = validated_data['email'], validated_data['name']
+        cpf, password = validated_data['cpf'], validated_data['password']
+        dealer = Dealer.objects.create_dealer(email=email, name=name, cpf=cpf, password=password)
         return dealer
 
 
 class PurchaseSerializer(serializers.ModelSerializer):
+    purchase_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", help_text='YYYY-MM-DD h:m:s')
+    cpf = serializers.CharField(validators=[validate_cpf, validate_dealer], max_length=11, min_length=11)
+
+    def create(self, validated_data):
+        cpf = validated_data.pop('cpf')
+        dealer = Dealer.objects.get(cpf=cpf)
+        purchase = Purchase.objects.create(dealer=dealer, **validated_data)
+        return purchase
+
     class Meta:
         model = Purchase
-        fields = '__all__'
+        fields = ('purchase_code', 'value', 'purchase_at', 'cpf')
+
--- core/validators.py
+++ core/validators.py
@@ -1,20 +1,13 @@
 
-from django.core.exceptions import ValidationError
-
-
-error_messages = {
-    'invalid': 'O cpf informado é inválido',
-    'digits_only': 'Digite apenas números',
-    'max_digits': 'O limite de caracteres foi excedido',
-}
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
 
 
 def validate_cpf(value):
     if not value.isdigit():
-        raise ValidationError('CPF should have only numbers.')
+        raise ValidationError('Cpf deve conter somente dígitos')
 
     if len(value) != 11:
-        raise ValidationError('CPF must be contains 11 digits')
+        raise ValidationError('CPF deve conter 11 dígitos')
 
     numbers = [int(digit) for digit in value]
 
@@ -22,10 +15,19 @@ def validate_cpf(value):
     sum_of_products = sum(a*b for a, b in zip(numbers[0:9], range(10, 1, -1)))
     expected_digit = (sum_of_products * 10 % 11) % 10
     if numbers[9] != expected_digit:
-        raise ValidationError('CPF is not valid.')
+        raise ValidationError('CPF inválido')
 
     # Validação do segundo dígito verificador:
     sum_of_products = sum(a*b for a, b in zip(numbers[0:10], range(11, 1, -1)))
     expected_digit = (sum_of_products * 10 % 11) % 10
     if numbers[10] != expected_digit:
-        raise ValidationError('CPF is not valid.')
+        raise ValidationError('CPF inválido')
+
+
+def validate_dealer(value):
+    from core.models import Dealer
+    try:
+        dealer = Dealer.objects.get(cpf=value)
+    except ObjectDoesNotExist:
+        raise ValidationError('Não existe revendedor cadastrado com esse CPF!')
+
