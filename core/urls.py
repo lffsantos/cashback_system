cccea6cd7893ae9fcf1801d0fa9d9f4822970422
Lucas <lffsantos@gmail.com>
2020-08-08 18:52:53 -0300

    refactor api

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from core.views import DealerViewSet
+
+from core.views import PurchaseViewSet, DealerRegisterViewSet
 
 app_name = 'core'
-#
+
 router = DefaultRouter()
-router.register(r'dealer', DealerViewSet)
+router.register(r'dealer', DealerRegisterViewSet)
+router.register(r'purcharse', PurchaseViewSet)
 urlpatterns = router.urls
--- core/views.py
+++ core/views.py
@@ -1,26 +1,25 @@
 from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.viewsets import GenericViewSet
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from core.models import Dealer, Purchase
 from core.serializers import DealerSerializer, PurchaseSerializer
 
 
-class DealerViewSet(viewsets.ModelViewSet):
-    http_method_names = ['get', 'post', 'put']
-    authentication_classes = [JSONWebTokenAuthentication, SessionAuthentication]
-    permission_classes = [IsAuthenticated]
+class DealerRegisterViewSet(GenericViewSet, CreateModelMixin):
+    http_method_names = ('post', )
+    permission_classes = (AllowAny,)
+    authentication_classes = []
     serializer_class = DealerSerializer
     queryset = Dealer.objects.all()
 
-    # def perform_destroy(self, instance):
-    #     user = instance.user
-    #     instance.delete()
-    #     user.delete()
-
 
 class PurchaseViewSet(viewsets.ModelViewSet):
-    http_method_names = ['get', 'post', 'put']
+    http_method_names = ['get', 'post']
+    authentication_classes = [JSONWebTokenAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = PurchaseSerializer
     queryset = Purchase.objects.all()
